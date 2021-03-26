fdba7b685b43e3e74a90706ede539e13bb59245b
MisokralPanovic <81387175+MisokralPanovic@users.noreply.github.com>
2021-03-26 18:23:37 +0000

    Update and rename hifit1.R to ddCt_values.R

--- Scripts/Templates/ddCt_values.R
+++ Scripts/Templates/ddCt_values.R
@@ -173,7 +173,7 @@ ggsave(filename = paste(file_name, '.png', sep = ''),
        units = 'cm')
 
 fwrite(hifit1, 
-       paste('Adjusted Data/', 
+       paste('Adjusted-Data/', 
              file_name, 
              '.csv', 
              sep = ''))
