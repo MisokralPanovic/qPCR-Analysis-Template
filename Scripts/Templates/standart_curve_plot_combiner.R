8b6f3c3591e273a95e54dd04d859e7a4323f9425
MisokralPanovic <81387175+MisokralPanovic@users.noreply.github.com>
2021-03-29 13:36:04 +0100

    Update standart_curve_plot_combiner.R

--- Scripts/Templates/standart_curve_plot_combiner.R
+++ Scripts/Templates/standart_curve_plot_combiner.R
@@ -14,18 +14,17 @@ file_name <- 'standard_curves'
 ##########
 
 combined_plot_x <- ggarrange(plot_standard1, plot_standard2, plot_standard3, plot_standard5, # what to plot
-                           labels = c('A', 'B', 'C', 'D'), # labels
-                           ncol = 2, nrow = 2, # dimension of new plot
-                           common.legend = T, legend = "bottom") # shared legend and legend position
+                             labels = c('A', 'B', 'C', 'D'), # labels
+                             ncol = 2, nrow = 2, # dimension of new plot
+                             common.legend = T, legend = "bottom") # shared legend and legend position
 
 combined_plot_x
 
-ggsave(filename = paste(file_name, '.png', sep = ''),
+ggsave(filename = paste(file_name, '.svg', sep = ''),
        plot = combined_plot, 
-       device = 'png', 
+       device = 'svg', 
        path = 'Figures/Combined-Plots', 
        dpi = dpi, 
        height = height, 
        width = width, 
        units = 'cm')
-
