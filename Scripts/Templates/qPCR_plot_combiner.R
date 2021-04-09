82be20ca820588b3701864e5fc1ca25e5a06ef95
MisokralPanovic <81387175+MisokralPanovic@users.noreply.github.com>
2021-04-09 14:33:02 +0100

    Update qPCR_plot_combiner.R

--- Scripts/Templates/qPCR_plot_combiner.R
+++ Scripts/Templates/qPCR_plot_combiner.R
@@ -15,7 +15,7 @@ file_name <- 'human_plots'
 
 
 combined_plot_x <- ggarrange(plot_a549_1, plot_a549_2, plot_a549_3, plot_a549_5, # what to plot
-                             labels = c('A', 'B', 'C', 'D'), # labels
+                             labels = 'auto, # labels
                              ncol = 4, nrow = 1, # dimension of new plot
                              common.legend = T, legend = "bottom") # shared legend and legend position
 
