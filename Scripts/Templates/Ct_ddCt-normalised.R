5982918c1ac79805a816740e068d1a18e102d6df
MisokralPanovic <michal.varga96@gmail.com>
2022-03-30 10:34:55 +0100

    improved plotting funcion

--- Scripts/Templates/Ct_ddCt-normalised.R
+++ Scripts/Templates/Ct_ddCt-normalised.R
@@ -2,27 +2,28 @@
 library(tidyverse)
 library(ggsignif)
 library(data.table)
+library(scales)
 
 figure_theme <-  theme(
   plot.title = element_text(
-    size = 20, 
+    size = 15, 
     face = 'bold', 
-    margin = margin(10, 0, 10, 0), 
+    margin = margin(8, 0, 8, 0), 
     hjust = 0.5
   ),
   legend.text = element_text(
-    size=15),  
+    size=10),  
   legend.title = element_blank(),
   axis.text.y = element_text(
     angle=0, 
-    size=12, 
+    size=9, 
     vjust=0.5),
   axis.title.x = element_blank(),
   axis.title.y = element_text(
-    size = 15, 
+    size = 12, 
     face='bold', 
     vjust=-0.5, 
-    margin = margin(0, 10, 0, 0)),
+    margin = margin(0, 8, 0, 0)),
   axis.text.x=element_blank(),
   axis.ticks.x=element_blank(),
   aspect.ratio = 2/1
@@ -34,20 +35,18 @@ figure_theme <-  theme(
 ###########
 list_of_conditions <- c(
   'Mock',
-  'cbRSV dNS1 0.001-24',
-  'cbRSV dNS2 0.01-24',
-  'cbRSV dNS1/2 0.001-24'
+  'hRSV 1-24 ROXO 5-24',
+  'hIFNa 1000-24'
   )
 list_of_colours <- c(
   '#999999', 
-  '#ffc034', 
   '#9a6a00', 
   '#009e73'
   )
 ###########
 
 # data prep ----
-gene_of_interest_data <- read.csv(
+gene_of_interest_data <- fread(
   paste('Data/', 
         
         ############
@@ -61,6 +60,7 @@ housekeeping_control <- gene_of_interest_data %>%
   
   ###########
   filter(Target == 'hGAPDH',
+         Cell_line == "A549",
          Condition %in% list_of_conditions
          ) %>%
   ###########
@@ -77,7 +77,7 @@ hifit1 <- gene_of_interest_data %>%
   ###########
   filter(
     Target == 'hIFIT1',
-    CellLine == 'A549',
+    Cell_line == "A549",
     Condition %in% list_of_conditions
          ) %>%
   ###########
@@ -109,7 +109,6 @@ plot(lm(Value_norm~Condition, hifit1))
 shapiro.test(hifit1$Value_norm[1:3])
 shapiro.test(hifit1$Value_norm[4:6])
 shapiro.test(hifit1$Value_norm[7:9])
-shapiro.test(hifit1$Value_norm[10:12])
 
 plot(residuals(
   lm(Value_norm~Condition, 
@@ -143,11 +142,8 @@ list_of_conditions
 ###########
 p_val <- c(
   0.8031417,
-  0.9655795,
-  0.0564754
+  0.0464754
   )
-range_y <- 20
-breaks_y <- 5
 plot_title <- 'A549 - hIFIT1'
 y_axis_title <- 'Relative mRNA Levels'
 ###########
@@ -157,10 +153,10 @@ textsize_values <- c()
 for (value in p_val) {
   if (value > 0.05) {
     textsize_values <- append(
-      textsize_values, 4)
+      textsize_values, 3)
   } else {
     textsize_values <- append(
-      textsize_values, 5)
+      textsize_values, 4)
   }
 }
 
@@ -186,24 +182,27 @@ plot_hifit1 <- ggplot(
   scale_fill_manual(
     breaks = list_of_conditions,
     values = list_of_colours) +
+  theme_bw() +
   figure_theme +
   labs(
     title = plot_title,
     y = y_axis_title,
     x = NULL
   ) +
-  coord_cartesian(ylim = c(0, range_y)) +
-  scale_y_continuous(
-    breaks= seq(0, 
-                range_y, 
-                breaks_y)) +
-  
+  scale_y_continuous(trans = log2_trans(),
+                     breaks = trans_breaks("log2", function(x) 2^x, n = 8),
+                     labels = trans_format("log2", math_format(2^.x)),
+                     limits = c(2^-7,2^9),
+                     sec.axis = sec_axis(trans = identity,
+                                         breaks = c(2^-6, 2^-4, 2^-2, 2^0, 2^2, 2^4, 2^6, 2^8),
+                                         labels = c(0.016, 0.062, 0.25, 1, 4, 16, 64, 256)
+                     )) +
   geom_signif(
     comparisons = list(c(
       list_of_conditions[1], 
       list_of_conditions[2])), 
     annotation = p_val[1], 
-    y_position = 0.93*range_y - 2*(range_y*0.075), 
+    y_position = 6, 
     tip_length = 0, 
     vjust= -0.2, 
     size = 0.7,
@@ -214,21 +213,12 @@ plot_hifit1 <- ggplot(
       list_of_conditions[1], 
       list_of_conditions[3])), 
     annotation = p_val[2], 
-    y_position = 0.93*range_y - 1*(range_y*0.075), 
+    y_position = 8, 
     tip_length = 0, 
     vjust= -0.2, 
     size = 0.7, 
-    textsize = textsize_values[2]) +
-  
-  geom_signif(
-    comparisons = list(c(list_of_conditions[1], 
-                         list_of_conditions[4])), 
-    annotation = p_val[3], 
-    y_position = 0.93*range_y - 0*(range_y*0.075), 
-    tip_length = 0, 
-    vjust= -0.2, 
-    size = 0.7, 
-    textsize = textsize_values[3])
+    textsize = textsize_values[2])
+
 
 plot_hifit1
 
