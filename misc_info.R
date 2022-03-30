b68e950feaf5b8341af4a239a17d0204c56b60d7
MisokralPanovic <michal.varga96@gmail.com>
2022-03-30 15:43:45 +0100

    r script with random usefull notes / functions

---
+++ misc_info.R
@@ -0,0 +1,29 @@
+library(broom)
+
+broom:glance(model)
+
+broom:tidy(model)
+
+broom:augment(model)
+
+# gets nested r oututs (models and so) into nice tibbles
+
+library(tidyverse)
+
+
+
+
+function(type = c("ddct", "extrapolation")) {
+  match.arg(type) # returns argument from type | if none is specified returns the first
+}
+  
+
+fun_name <- source("path/function_file.R")
+
+# make a plotting function
+
+
+install.packages("pkgbuild", dependencies = TRUE)
+
+
+methods(class = "GRanges")
