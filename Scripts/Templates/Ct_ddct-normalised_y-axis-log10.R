98294b5876bab3d457bb5a102f6bab6a5c769336
MisokralPanovic <michal.varga96@gmail.com>
2022-03-30 11:02:48 +0100

    chanbging ploting function + some extras

--- Scripts/Templates/Ct_ddct-normalised_y-axis-log10.R
+++ Scripts/Templates/Ct_ddct-normalised_y-axis-log10.R
@@ -2,56 +2,49 @@
 library(tidyverse)
 library(ggsignif)
 library(data.table)
-library(ggsignif)
 library(scales)
-library(ggplot2)
-library(ggpubr)
 
-figure_theme <-theme(
+figure_theme <-  theme(
   plot.title = element_text(
-    size = 20, 
+    size = 15, 
     face = 'bold', 
-    margin = margin(10, 0, 10, 0), 
+    margin = margin(8, 0, 8, 0), 
     hjust = 0.5
   ),
   legend.text = element_text(
-    size=15),  
+    size=10),  
   legend.title = element_blank(),
   axis.text.y = element_text(
     angle=0, 
-    size=12, 
+    size=9, 
     vjust=0.5),
   axis.title.x = element_blank(),
   axis.title.y = element_text(
-    size = 15, 
+    size = 12, 
     face='bold', 
     vjust=-0.5, 
-    margin = margin(0, 10, 0, 0)),
+    margin = margin(0, 8, 0, 0)),
   axis.text.x=element_blank(),
   axis.ticks.x=element_blank(),
   aspect.ratio = 2/1
 )
 ###########
-# change 'hRSV N', ' 'mdbk_brsvn_48h'
+# change 'bRSV N', ' 'mdbk_brsv'
 ###########
 
 ###########
 list_of_conditions <- c(
   'Mock',
-  'cbRSV 0.1-48',
-  'cbRSV 1-48',
-  'cbRSV 2-48'
+  'bRSV dSH 1-24'
 )
 list_of_colours <- c(
   '#999999', 
-  '#e200e2', 
-  '#a800a8', 
   '#6d006d'
 )
 ###########
 
 # data prep ----
-gene_of_interest_data <- read.csv(
+gene_of_interest_data <- fread(
   paste('Data/', 
         
         ############
@@ -65,8 +58,7 @@ housekeeping_control <- gene_of_interest_data %>%
   
   ###########
   filter(Target == 'bGAPDH',
-        Time_point == '48h',
-        Additional_info != 'bMx1',
+         Cell_line == "MDBK",
         Condition %in% list_of_conditions
 ) %>%
   ###########
@@ -78,12 +70,12 @@ housekeeping_control <- gene_of_interest_data %>%
     Condition, 
     list_of_conditions))
 
-mdbk_brsvn_48h <- gene_of_interest_data %>%
+mdbk_brsv <- gene_of_interest_data %>%
   
   ###########
 filter(
   Target == 'bRSV_N',
-  Time_point == '48h',
+  Cell_line == "MDBK",
   Condition %in% list_of_conditions
 ) %>% 
   ###########
@@ -105,29 +97,27 @@ arrange(match(
     Value_norm = log2_ddct / mock_mean_log
   )
 
-mdbk_brsvn_48h
+mdbk_brsv
 
 # data analysis ----
-boxplot(Value_norm~Condition, mdbk_brsvn_48h)
-plot(lm(Value_norm~Condition, mdbk_brsvn_48h))
+boxplot(Value_norm~Condition, mdbk_brsv)
+plot(lm(Value_norm~Condition, mdbk_brsv))
 
 # test normality
-shapiro.test(mdbk_brsvn_48h$Value_norm[1:3])
-shapiro.test(mdbk_brsvn_48h$Value_norm[4:6])
-shapiro.test(mdbk_brsvn_48h$Value_norm[7:9])
-shapiro.test(mdbk_brsvn_48h$Value_norm[10:12])
+shapiro.test(mdbk_brsv$Value_norm[1:3])
+shapiro.test(mdbk_brsv$Value_norm[4:6])
 
 plot(residuals(
   lm(Value_norm~Condition, 
-     mdbk_brsvn_48h)))
+     mdbk_brsv)))
 shapiro.test(residuals(
   lm(Value_norm~Condition, 
-     mdbk_brsvn_48h)))
+     mdbk_brsv)))
 
 ### normal distribution
 
 # if distribution normal
-bartlett.test(Value_norm~Condition, mdbk_brsvn_48h)
+bartlett.test(Value_norm~Condition, mdbk_brsv)
 
 ### non equal variance
 
@@ -135,23 +125,21 @@ bartlett.test(Value_norm~Condition, mdbk_brsvn_48h)
 library(rstatix)
 library(dplyr)
 oneway.test(Value_norm~Condition, 
-            mdbk_brsvn_48h, 
+            mdbk_brsv, 
             var.equal = F)
-mdbk_brsvn_48h %>% games_howell_test(Value_norm~Condition)
+mdbk_brsv %>% games_howell_test(Value_norm~Condition)
 
 # plot data ----
-boxplot(Value_norm~Condition, mdbk_brsvn_48h)
-mdbk_brsvn_48h
+boxplot(Value_norm~Condition, mdbk_brsv)
+mdbk_brsv
 list_of_conditions
 
 ###########
 p_val <- c(
-  0.076,
-  0.137,
-  0.032
+  0.036,
+  0.232
 )
-range_y <- 9
-plot_title <- 'MDBK - 48h - bRSV N'
+plot_title <- 'MDBK - bRSV N'
 y_axis_title <- 'Relative mRNA Levels'
 ###########
 
@@ -160,15 +148,15 @@ textsize_values <- c()
 for (value in p_val) {
   if (value > 0.05) {
     textsize_values <- append(
-      textsize_values, 4)
+      textsize_values, 3)
   } else {
     textsize_values <- append(
-      textsize_values, 5)
+      textsize_values, 4)
   }
 }
 
-plot_mdbk_brsvn_48h <- ggplot(
-  mdbk_brsvn_48h, 
+plot_mdbk_brsv <- ggplot(
+  mdbk_brsv, 
   aes(
     x = Condition, 
     y = Value_norm,
@@ -189,6 +177,7 @@ plot_mdbk_brsvn_48h <- ggplot(
   scale_fill_manual(
     breaks = list_of_conditions,
     values = list_of_colours) +
+  theme_bw() +
   figure_theme +
   labs(
     title = plot_title,
@@ -198,7 +187,8 @@ plot_mdbk_brsvn_48h <- ggplot(
   scale_y_log10(labels = trans_format("log10", 
                                       math_format(10^.x)),
                 breaks = trans_breaks("log10", 
-                                      function(x) 10^x)
+                                      function(x) 10^x, n = 7),
+                limits = c(10^-1, 10^9)
   ) +
   annotation_logticks(sides='l') +
   geom_signif(
@@ -206,34 +196,13 @@ plot_mdbk_brsvn_48h <- ggplot(
       list_of_conditions[1], 
       list_of_conditions[2])), 
     annotation = p_val[1], 
-    y_position = 0.93*range_y - 2*(range_y*0.075), 
+    y_position = 7, 
     tip_length = 0, 
     vjust= -0.2, 
     size = 0.7,
-    textsize = textsize_values[1]) +
-  
-  geom_signif(
-    comparisons = list(c(
-      list_of_conditions[1], 
-      list_of_conditions[3])), 
-    annotation = p_val[2], 
-    y_position = 0.93*range_y - 1*(range_y*0.075), 
-    tip_length = 0, 
-    vjust= -0.2, 
-    size = 0.7, 
-    textsize = textsize_values[2]) +
-  
-  geom_signif(
-    comparisons = list(c(list_of_conditions[1], 
-                         list_of_conditions[4])), 
-    annotation = p_val[3], 
-    y_position = 0.93*range_y - 0*(range_y*0.075), 
-    tip_length = 0, 
-    vjust= -0.2, 
-    size = 0.7, 
-    textsize = textsize_values[3])
+    textsize = textsize_values[1])
 
-plot_mdbk_brsvn_48h
+plot_mdbk_brsv
 
 # saving data and plot ------------------------------
 
@@ -241,12 +210,12 @@ plot_mdbk_brsvn_48h
 dpi <- 600
 width <- 16
 height <- 20
-file_name <- 'mdbk_brsvn_48h'
+file_name <- 'mdbk_brsv'
 ###########
 
 ggsave(filename = paste(
   file_name, '.svg', sep = ''), 
-  plot = plot_mdbk_brsvn_48h, 
+  plot = plot_mdbk_brsv, 
   device = 'svg', 
   path = 'Figures', 
   dpi = dpi, 
@@ -255,7 +224,7 @@ ggsave(filename = paste(
   units = 'cm')
 ggsave(filename = paste(
   file_name, '.png', sep = ''), 
-  plot = plot_mdbk_brsvn_48h, 
+  plot = plot_mdbk_brsv, 
   device = 'png', 
   path = 'Figures', 
   dpi = dpi, 
@@ -263,7 +232,7 @@ ggsave(filename = paste(
   width = width, 
   units = 'cm')
 
-fwrite(mdbk_brsvn_48h, 
+fwrite(mdbk_brsv, 
        paste('Adjusted-Data/', 
              file_name, 
              '.csv', 
